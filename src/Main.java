1663311554cb01455ac693376c2fed18ef92c396
williamGanem <usr3852@salesianos-lacuesta.net>
2024-04-02 12:35:43 +0100

    feat(Main): ask number to user and print it

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        Scanner scanner = new Scanner(System.in);
+        int askNumber = scanner.nextInt();
+        System.out.println(askNumber);
+
     }
 
 }
